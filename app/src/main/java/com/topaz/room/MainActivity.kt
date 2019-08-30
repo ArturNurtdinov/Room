90614110f6a998869e265769ca4e849f91be3b78
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-08-30 15:21:53 +0300

    Added LiveData and Coroutines

--- app/src/main/java/com/topaz/room/MainActivity.kt
+++ app/src/main/java/com/topaz/room/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.topaz.room
 
+import android.arch.lifecycle.Observer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.topaz.room.data.Employee
 import com.topaz.room.database.AppDatabase
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         val db = AppDatabase.getInstance(this)
         val dao = db.dao
-        dao.insert(Employee(1, "Art", 555))
+        dao.insert(Employee(5, "Art", 555))
 
-        val data = dao.getAll()
-        data.forEach {
-            Log.d("MYDB", "${it.id} ${it.name} ${it.salary}")
+        CoroutineScope(Dispatchers.Default).launch {
+            dao.getAll().observe(this@MainActivity, object : Observer<List<Employee>> {
+                override fun onChanged(t: List<Employee>?) {
+                    text_view.text = t?.size.toString()
+                }
+
+            })
         }
     }
 }
--- app/src/main/java/com/topaz/room/data/EmployeeDao.kt
+++ app/src/main/java/com/topaz/room/data/EmployeeDao.kt
@@ -1,11 +1,12 @@
 package com.topaz.room.data
 
+import android.arch.lifecycle.LiveData
 import android.arch.persistence.room.*
 
 @Dao
 interface EmployeeDao {
     @Query("SELECT * FROM employee")
-    fun getAll(): List<Employee>
+    fun getAll(): LiveData<List<Employee>>
 
     @Query("SELECT * FROM employee WHERE id = :id")
     fun getById(id: Int): Employee
